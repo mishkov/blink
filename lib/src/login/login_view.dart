70588637850801a9a8ef1775baf05eab8cc9182d
Nikita Mishkov <mishkov.dd@gmail.com>
2022-02-23 19:18:54 +0300

    Rewrite LoginView to LoginBloc

--- lib/src/login/login_view.dart
+++ lib/src/login/login_view.dart
@@ -3,6 +3,8 @@ import 'package:blink/src/home/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'login_bloc.dart';
+
 class LoginView extends StatelessWidget {
   static const routeName = '/login';
 
@@ -11,19 +13,108 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocListener<UserBloc, UserState>(
-        listener: (context, state) {
-          Navigator.popAndPushNamed(context, HomeView.routeName);
-        },
-        child: Center(
+      body: BlocBuilder<LoginBloc, LoginState>(builder: (context, state) {
+        return Center(
           child: ElevatedButton(
             onPressed: () {
               context.read<UserBloc>().loginWithGoogle();
             },
             child: const Text('Login with Google'),
           ),
-        ),
-      ),
+        );
+      }
+          // listener: (context, state) {
+          //   if (state is UserInLogin && state.inProgress) {
+          //     showDialog<void>(
+          //       context: context,
+          //       barrierDismissible: false,
+          //       builder: (BuildContext context) {
+          //         return AlertDialog(
+          //           title: const Text('Login'),
+          //           content: Center(
+          //             child: Column(
+          //               mainAxisAlignment: MainAxisAlignment.center,
+          //               children: const [
+          //                 CircularProgressIndicator(),
+          //                 Text('Please wait for login...'),
+          //               ],
+          //             ),
+          //           ),
+          //         );
+          //       },
+          //     );
+          //   } else if (state is ErrorUserState) {
+          //     showDialog<void>(
+          //       context: context,
+          //       barrierDismissible: true,
+          //       builder: (BuildContext context) {
+          //         return AlertDialog(
+          //           title: const Text('Error'),
+          //           content: Center(
+          //             child: Column(
+          //               mainAxisAlignment: MainAxisAlignment.center,
+          //               children: [
+          //                 const Icon(
+          //                   Icons.error_outline,
+          //                   color: Colors.red,
+          //                 ),
+          //                 Text(state.message),
+          //               ],
+          //             ),
+          //           ),
+          //           actions: [
+          //             TextButton(
+          //               child: const Text('Try again'),
+          //               onPressed: () {
+          //                 Navigator.of(context).pop();
+          //                 context.read<UserBloc>().loginWithGoogle();
+          //               },
+          //             ),
+          //             TextButton(
+          //               child: const Text('OK'),
+          //               onPressed: () {
+          //                 Navigator.of(context).pop();
+          //               },
+          //             ),
+          //           ],
+          //         );
+          //       },
+          //     );
+          //   } else if (state is ReadyUserState) {
+          //     Navigator.popAndPushNamed(context, HomeView.routeName);
+          //   } else {
+          //     showDialog<void>(
+          //       context: context,
+          //       barrierDismissible: false,
+          //       builder: (BuildContext context) {
+          //         return AlertDialog(
+          //           title: const Text('Error'),
+          //           content: Center(
+          //             child: Column(
+          //               mainAxisAlignment: MainAxisAlignment.center,
+          //               children: const [
+          //                 Icon(
+          //                   Icons.error_outline,
+          //                   color: Colors.red,
+          //                 ),
+          //                 Text('Unkkown error happend'),
+          //               ],
+          //             ),
+          //           ),
+          //           actions: [
+          //             TextButton(
+          //               child: const Text('OK'),
+          //               onPressed: () {
+          //                 Navigator.of(context).pop();
+          //               },
+          //             ),
+          //           ],
+          //         );
+          //       },
+          //     );
+          //   }
+          // },
+          ),
     );
   }
 }
