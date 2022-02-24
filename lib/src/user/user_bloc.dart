06003d233db69a6cc006815c96bf3be3148fdc9c
Nikita Mishkov <mishkov.dd@gmail.com>
2022-02-24 21:09:02 +0300

    Remove unnecessary import

--- lib/src/user/user_bloc.dart
+++ lib/src/user/user_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
