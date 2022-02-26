78343bc7514e8a807a32d2fed0efb65ba4328f61
Nikita Mishkov <mishkov.dd@gmail.com>
2022-02-26 15:05:42 +0300

    Add local camera service

--- lib/src/home/home_model_view.dart
+++ lib/src/home/home_model_view.dart
@@ -1,20 +1,60 @@
+import 'package:blink/src/local_camera/local_camera_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 class HomeModelView extends Cubit<HomeState> {
-  double _bidInDollars = 1;
+  final _videoService = LocalCameraService();
+  int _bidInDollars = 1;
 
-  HomeModelView() : super(HomeState());
+  HomeModelView()
+      : super(
+          HomeState(
+            bidInDollars: 1,
+            eyesOpenStream: LocalCameraService().eyesOpenStream,
+            localVideo: LocalCameraService().video,
+          ),
+        ) {
+    _init();
+  }
 
-  set bidInDollars(double bid) {
-    if (bid < 0.0) {
+  set bidInDollars(int? bid) {
+    if (bid == null) {
+      // TODO: create special exception for this case
+      throw Exception('wrong bid format');
+    }
+    if (bid.isNegative) {
+      // TODO: create special exception for this case
       throw Exception('bid can not be negative');
     }
     _bidInDollars = bid;
   }
 
-  double get bidInDollars => _bidInDollars;
+  int get bidInDollars => _bidInDollars;
+
+  Future<void> _init() async {
+    await _videoService.initLocalRenderer();
+    await _videoService.initEyesOpenStream();
+
+    emit(HomeState(
+      bidInDollars: _bidInDollars,
+      eyesOpenStream: _videoService.eyesOpenStream,
+      localVideo: _videoService.video,
+    ));
+  }
 }
 
 class HomeState {
-  HomeState();
+  Stream<dynamic>? eyesOpenStream;
+  RTCVideoRenderer? localVideo;
+  bool errorHappened;
+  String? errorMessage;
+  int? bidInDollars;
+
+  HomeState({
+    this.bidInDollars,
+    this.eyesOpenStream,
+    this.localVideo,
+    this.errorHappened = false,
+    this.errorMessage,
+  });
 }
