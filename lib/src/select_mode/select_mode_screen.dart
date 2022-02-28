1c7aea85315450bb72bc8caa2bc9c3af9eb4a6e0
Nikita Mishkov <mishkov.dd@gmail.com>
2022-02-28 17:58:27 +0300

    Refactor widget tree

--- lib/src/select_mode/select_mode_screen.dart
+++ lib/src/select_mode/select_mode_screen.dart
@@ -8,168 +8,149 @@ class SelectModeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Refactor widgets
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              IntrinsicHeight(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 4),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12.0),
-                              child: const Image(
-                                image: AssetImage(
-                                  'assets/images/squid_game_player.jpg',
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: Text(
-                                AppLocalizations.of(context)!.realManMode,
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 20, bottom: 40),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // TODO: Implement navigation to the battle
-                                  // with real man screen
-                                },
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all(
-                                      const Color.fromARGB(255, 255, 212, 155)),
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(9.0),
-                                    ),
-                                  ),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 14.0,
-                                    horizontal: 24,
-                                  ),
-                                  child: Text(
-                                    AppLocalizations.of(context)!.playButton,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 4),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(12.0),
-                              child: const Image(
-                                image: AssetImage(
-                                  'assets/images/robot_doll.jpeg',
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8),
-                              child:
-                                  Text(AppLocalizations.of(context)!.botMode),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 20, bottom: 40),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // TODO: Implement navigation to the battle
-                                  // with bot screen
-                                },
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all(
-                                      const Color.fromARGB(255, 206, 73, 69)),
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(9.0),
-                                    ),
-                                  ),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 14.0,
-                                    horizontal: 24,
-                                  ),
-                                  child: Text(
-                                    AppLocalizations.of(context)!.playButton,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20, bottom: 40),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(9.0),
-                      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Mode(
+                      imagePath: 'assets/images/squid_game_player.jpg',
+                      desctiption: AppLocalizations.of(context)!.realManMode,
+                      buttonColor: const Color.fromARGB(255, 255, 212, 155),
+                      onPressed: () {
+                        // TODO: Implement navigation to the battle
+                        // with real man screen
+                      },
                     ),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 14.0,
-                      horizontal: 24,
-                    ),
-                    child: Text(
-                      AppLocalizations.of(context)!.homeButton,
-                      style: const TextStyle(
-                        fontSize: 14,
-                      ),
+                  const SizedBox(width: 4),
+                  Expanded(
+                    child: Mode(
+                      imagePath: 'assets/images/robot_doll.jpeg',
+                      desctiption: AppLocalizations.of(context)!.botMode,
+                      buttonColor: const Color.fromARGB(255, 206, 73, 69),
+                      onPressed: () {
+                        // TODO: Implement navigation to the battle
+                        // with bot screen
+                      },
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
+            const HomeButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeButton extends StatelessWidget {
+  const HomeButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 40),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(9.0),
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 14.0,
+            horizontal: 24,
+          ),
+          child: Text(
+            AppLocalizations.of(context)!.homeButton,
+            style: const TextStyle(
+              fontSize: 14,
+            ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class Mode extends StatelessWidget {
+  final String imagePath;
+  final String desctiption;
+  final Color buttonColor;
+  final void Function()? onPressed;
+
+  const Mode({
+    required this.imagePath,
+    Key? key,
+    required this.desctiption,
+    required this.buttonColor,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: Image(
+            image: AssetImage(
+              imagePath,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Text(
+            desctiption,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 20, bottom: 40),
+          child: ElevatedButton(
+            onPressed: onPressed,
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(buttonColor),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0),
+                ),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 14.0,
+                horizontal: 24,
+              ),
+              child: Text(
+                AppLocalizations.of(context)!.playButton,
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
