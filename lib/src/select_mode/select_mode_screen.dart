46b39fb8f5e5a60cf4dd31a9dd0228db62f1f024
Nikita Mishkov <mishkov.dd@gmail.com>
2022-02-28 17:42:27 +0300

    Remove unused import

--- lib/src/select_mode/select_mode_screen.dart
+++ lib/src/select_mode/select_mode_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import '../home/home_view.dart';
-
 class SelectModeScreen extends StatelessWidget {
   static const routeName = '/select_mode';
 
