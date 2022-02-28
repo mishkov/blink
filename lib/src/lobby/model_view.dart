35aaf328051ade93e767d43cd8014b5175c0daf1
Nikita Mishkov <mishkov.dd@gmail.com>
2022-02-28 12:17:34 +0300

    Override close method

--- lib/src/lobby/model_view.dart
+++ lib/src/lobby/model_view.dart
@@ -1,7 +1,17 @@
+import 'package:blink/src/signaling/signaling_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 class LobbyModelView extends Cubit<LobbyState> {
-  LobbyModelView() : super(LobbyState());
+  LobbyModelView({MediaStream? localStream}) : super(LobbyState()) {
+    SignalingService().localStream = localStream;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: implement close
+    return super.close();
+  }
 }
 
 class LobbyState {}
