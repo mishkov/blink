88f7ed226c2dad1d1a448eed105674e10c02303b
Nikita Mishkov <mishkov.dd@gmail.com>
2022-03-04 18:47:03 +0300

    Implement highest time convertion

--- lib/src/drawer/app_drawer_view_model.dart
+++ lib/src/drawer/app_drawer_view_model.dart
@@ -1,22 +1,72 @@
+import 'dart:async';
+
 import 'package:blink/src/user/user.dart';
 import 'package:blink/src/user/user_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppDrawerViewModel extends Cubit<AppDrawerState> {
+  StreamSubscription? _userStreamSubscription;
+
   AppDrawerViewModel()
-      : super(AppDrawerState(user: UserService().user ?? User())) {
+      : super(AppDrawerState(
+          name: UserService().user?.name ?? '',
+          photoUrl: UserService().user?.photoUrl ?? '',
+          email: UserService().user?.email ?? '',
+          highestTime: _timeFromMilliseconds(UserService().user?.highestTime),
+          balance: (UserService().user?.balance ?? '').toString(),
+          won: (UserService().user?.won ?? '').toString(),
+          lost: (UserService().user?.lost ?? '').toString(),
+        )) {
     _registerListener();
   }
 
   void _registerListener() {
-    UserService().userStream.listen((user) {
-      emit(AppDrawerState(user: user));
+    _userStreamSubscription = UserService().userStream.listen((user) {
+      final state = AppDrawerState(
+        name: user.name ?? '',
+        photoUrl: user.photoUrl ?? '',
+        email: user.email ?? '',
+        highestTime: _timeFromMilliseconds(user.highestTime),
+        balance: (user.balance ?? '').toString(),
+        won: (user.won ?? '').toString(),
+        lost: (user.lost ?? '').toString(),
+      );
+      emit(state);
     });
+  }
+
+  @override
+  Future<void> close() async {
+    await _userStreamSubscription?.cancel();
+    return super.close();
+  }
+
+  static String _timeFromMilliseconds(int? timeInMilliseconds) {
+    final time = DateTime.fromMillisecondsSinceEpoch(timeInMilliseconds ?? 0);
+    final minutes = time.minute;
+    final seconds = time.second;
+    final milliseconds = time.millisecond ~/ 100;
+
+    return '$minutes:$seconds:$milliseconds';
   }
 }
 
 class AppDrawerState {
-  final User user;
+  final String name;
+  final String photoUrl;
+  final String email;
+  final String highestTime;
+  final String balance;
+  final String won;
+  final String lost;
 
-  AppDrawerState({required this.user});
+  AppDrawerState({
+    required this.name,
+    required this.photoUrl,
+    required this.email,
+    required this.highestTime,
+    required this.balance,
+    required this.won,
+    required this.lost,
+  });
 }
